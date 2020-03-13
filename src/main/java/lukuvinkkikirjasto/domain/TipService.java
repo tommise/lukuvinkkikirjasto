d85c8830ecc1a1c4909f5ddf32f21efd125ca0ed
Tambourin <jirka@gmail.com>
2020-03-13 08:11:42 +0200

    TipService aluille

--- src/main/java/lukuvinkkikirjasto/domain/TipService.java
+++ src/main/java/lukuvinkkikirjasto/domain/TipService.java
@@ -1,6 +1,33 @@
 
 package lukuvinkkikirjasto.domain;
 
+import java.sql.SQLException;
+
+import lukuvinkkikirjasto.dao.TipDao;
+
 public class TipService {
-    
+  private TipDao tipDao;
+
+  public TipService(TipDao tipDao) {
+    this.tipDao = tipDao;
+  }
+
+  public Tip createTip(String title, String link) {
+    if (!tipTitleIsValid(title)) {
+      return null;
+    }
+
+    try {
+      Tip tip = tipDao.create(title, link);
+      return tip;
+    } catch (SQLException e) {
+      e.printStackTrace();
+      return null; 
+    }       
+  }
+
+
+  private boolean tipTitleIsValid(String title){
+    return title.length() > 0;
+  }
 }
