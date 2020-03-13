7b244743c1c3234e011e2c0dd7c98f9f28cd399c
Tambourin <jirka@gmail.com>
2020-03-13 19:59:49 +0200

    Tyylikorjailua

--- src/test/java/lukuvinkkikirjasto/domain/TipServiceTest.java
+++ src/test/java/lukuvinkkikirjasto/domain/TipServiceTest.java
@@ -6,32 +6,50 @@ import lukuvinkkikirjasto.dao.TipDao;
 
 import static org.junit.Assert.*;
 
+import java.sql.SQLException;
+
 public class TipServiceTest {
-  TipService tipService;
+    TipService tipService;
 
-  TipDao stubTipDao = new TipDao(){  
-    @Override
-    public Tip create(String title, String link){      
-      return new Tip(title, link, 1);
+    TipDao stubTipDao = new TipDao(){  
+        
+        @Override
+        public Tip create(String title, String link){      
+        return new Tip(title, link, 1);
+        }
+    };
+
+    TipDao stubTipDaoThrowsException = new TipDao(){
+        
+        @Override
+        public Tip create(String title, String link) throws SQLException {
+        throw new SQLException("error");
+        }
+    };
+
+    @Before
+    public void init() {
+        this.tipService = new TipService(stubTipDao);
     }
-  };
 
-  @Before
-  public void init() {
-    this.tipService = new TipService(stubTipDao);
-  }
+    @Test
+    public void tipServiceReturnsTipObject() {
+        Tip tip = tipService.createTip("title", "link");
+        assertEquals("title", tip.getTitle());
+        assertEquals("link", tip.getLink());
+    }
 
-  @Test
-  public void tipServiceReturnsTipObject() {
-    Tip tip = tipService.createTip("title", "link");
-    assertEquals("title", tip.getTitle());
-    assertEquals("link", tip.getLink());
-  }
+    @Test
+    public void ifEmptyTitleIsGivenTipServiceReturnsNull() {
+        Tip tip = tipService.createTip("", "link");
+        assertEquals(null, tip);
+    }
 
-  @Test
-  public void ifEmptyTitleIsGivenTipServiceReturnsNull() {
-    Tip tip = tipService.createTip("", "link");
-    assertEquals(null, tip);
-  }
+    @Test
+    public void tipServiceCatchesException() {
+        TipService service = new TipService(stubTipDaoThrowsException);
+        Tip tip = service.createTip("title", "link");
+        assertEquals(null, tip);
+    }
 
 }
