124cf1437a98f96c0f058c4464880ca2f77978a9
Taru Saloheimo <taru.saloheimo@gmail.com>
2020-03-14 21:24:16 +0200

    jos otsikkoa ei annettu, sitä kysytään uudelleen

--- src/main/java/lukuvinkkikirjasto/ui/IOService.java
+++ src/main/java/lukuvinkkikirjasto/ui/IOService.java
@@ -1,5 +1,8 @@
 package lukuvinkkikirjasto.ui;
 
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.TreeMap;
 import lukuvinkkikirjasto.dao.Database;
 import lukuvinkkikirjasto.dao.SqlTipDao;
 import lukuvinkkikirjasto.dao.TipDao;
@@ -9,30 +12,72 @@ public class IOService {
 
     private IO io;
     private TipService tipService;
+    private Map<String, String> commands;
     
     public IOService(IO io) {
         this.io = io;
+        this.commands = createCommands();
     }
+    
+    private TreeMap<String, String> createCommands() {
+        TreeMap commands = new TreeMap<>();
+        commands.put("1", "1: luo uusi lukuvinkki");
+        commands.put("2", "2: tarkastele vinkkejä");
+        return commands;
+    }    
     
     public void suorita() {
-        instructions();
+        printInstructions();
+        
         while (true) {
+            io.print("Komento: "); 
+            String command = io.nextLine();
+            System.out.println("");
+            if (!commands.keySet().contains(command)) {
+                System.out.println("Komentoa ei tunnistettu.");
+                printInstructions();
+            }
+            if (command.equals("1")) {
+                createTip();
+            }
             
-            io.print("Otsikko: ");
-            String otsikko = io.nextLine();
-            io.print("Linkki: ");
-            String linkki = io.nextLine();
-            createTip(otsikko, linkki);
-            break;
-            
-        }
+        }       
     }
     
-    private void instructions() {
-        io.print("Tervetuloa lukuvinkkisovellukseen! \n"
-                + "Lisää uusi vinkki, valitse 1 \n"
-                + "Tarkastele lukuvinkkejä, valitse 2");
+    private void createTip() {
+        String otsikko = null;
+        while (otsikko == null) {
+            io.print("Otsikko: ");
+            otsikko = io.nextLine();
+            otsikko = validateTitle(otsikko);
+        }
+        io.print("Linkki: ");
+        String linkki = io.nextLine();
+        
+        tipService.createTip(otsikko, linkki);
+        
+        io.print("Lukuvinkki lisätty!");
+        io.print("");
     }
+    
+    private String validateTitle(String input) {
+        String trimmedInput = input.trim();
+        if (trimmedInput.length() == 0) {
+            io.print("Otsikko ei voi olla tyhjä!");
+            trimmedInput = null;
+        }
+        return trimmedInput;
+    }
+    
+    private void printInstructions() {
+        System.out.println("Valitse joku seuraavista komennoista:");
+        for (Map.Entry<String, String> entry : commands.entrySet()) {
+            io.print(entry.getValue());
+        }    
+        io.print("");
+    }
+    
+    
     
     private void createTip(String otsikko, String linkki) {
         tipService.createTip(otsikko, linkki);
