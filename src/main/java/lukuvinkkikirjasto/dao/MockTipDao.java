fee912702bb74ca2e12141cde6208f83b6bf0550
Tambourin <jirka@gmail.com>
2020-03-16 11:31:26 +0200

    MockTipDao2

---
+++ src/main/java/lukuvinkkikirjasto/dao/MockTipDao.java
@@ -0,0 +1,24 @@
+package lukuvinkkikirjasto.dao;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import lukuvinkkikirjasto.domain.Tip;
+
+public class MockTipDao implements TipDao {
+    private List<Tip> tips = new ArrayList<>();
+
+    @Override
+    public Tip create(String title, String link) throws SQLException {
+        Tip tip = new Tip(title, link, 1);
+        tips.add(tip);
+        return tip;
+    }
+
+    @Override
+    public List<Tip> getAll() throws SQLException {
+        return tips;
+    }
+    
+}
--- src/main/java/lukuvinkkikirjasto/ui/IOService.java
+++ src/main/java/lukuvinkkikirjasto/ui/IOService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import lukuvinkkikirjasto.dao.Database;
-import lukuvinkkikirjasto.dao.MockTipDao;
 import lukuvinkkikirjasto.dao.SqlTipDao;
 import lukuvinkkikirjasto.dao.TipDao;
 import lukuvinkkikirjasto.domain.Tip;
